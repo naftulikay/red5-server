f92f807bfd6e95d4827daa0db3978812060d8401
pgregoire <pgregoire@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-09-12 06:24:25 +0000

    implementation of oflaDemo in javascript / rhino

---
+++ webapps/oflaDemo/WEB-INF/classes/applications/demoservice.js
@@ -0,0 +1,68 @@
+/*
+ * demoservice.js - a translation into JavaScript of the olfa demo DemoService class, a red5 example.
+ *
+ * @see http://developer.mozilla.org/en/docs/Core_JavaScript_1.5_Reference
+ * @author Paul Gregoire
+ */
+
+var javaNames = JavaImporter();
+
+javaNames.importPackage(Packages.org.red5.server.api);
+javaNames.importPackage(Packages.org.springframework.core.io);
+
+function DemoService() {
+
+	function getListOfAvailableFLVs() {
+		var scope = Red5.getConnectionLocal().getScope();
+		var filesMap = new HashMap();
+		var fileInfo;
+		try {
+			print('Getting the FLV files');
+			var flvs = scope.getResources("streams/*.flv"); //Resource[]
+			for (var flv in flvs) {
+				var file = flv.getFile();
+				var lastModified = formatDate(new Date(file.lastModified()));
+				var flvName = file.getName();
+				var flvBytes = file.length();
+	
+				print('FLV Name: ' + flvName);
+				print('Last modified date: ' + lastModified);
+				print('Size: ' + flvBytes);
+				print('-------');
+				
+				fileInfo = new HashMap();
+				fileInfo.put("name", flvName);
+				fileInfo.put("lastModified", lastModified);
+				fileInfo.put("size", flvBytes);
+				filesMap.put(flvName, fileInfo);
+			}
+			print('Getting the MP3 files');
+			var mp3s = scope.getResources("streams/*.mp3");
+			for (var mp3 in mp3s) {
+				var file = mp3.getFile();
+				var lastModified = formatDate(new Date(file.lastModified()));
+				var mpName = file.getName();
+				var mpBytes = file.length();
+	
+				print('MP3 Name: ' + mpName);
+				print('Last modified date: ' + lastModified);
+				print('Size: ' + mpBytes);
+				print('-------');
+				
+				fileInfo = new HashMap();
+				fileInfo.put("name", mpName);
+				fileInfo.put("lastModified", lastModified);
+				fileInfo.put("size", mpBytes);
+				filesMap.put(mpName, fileInfo);
+			}
+		} catch (e) {
+			print('Exception: ' + e);
+		}
+		return filesMap;
+	}
+
+	function formatDate(date) {
+		return new String(date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
+	}
+	
+}
--- webapps/oflaDemo/WEB-INF/classes/applications/main.js
+++ webapps/oflaDemo/WEB-INF/classes/applications/main.js
@@ -1,5 +1,5 @@
 /*
- * application.js - a translation into JavaScript of the olfa demo application, a red5 example.
+ * main.js - a translation into JavaScript of the olfa demo Application class, a red5 example.
  *
  * @author Paul Gregoire
  */

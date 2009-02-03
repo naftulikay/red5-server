6f315ab382b779fb3df74270defa14b920d0980b
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2009-02-03 23:01:21 +0000

    added .class missing from loader mbean. removed cp override in shutdown.bat. updated debug flag

--- red5-debug.bat
+++ red5-debug.bat
@@ -2,5 +2,5 @@
 
 if NOT DEFINED RED5_HOME set RED5_HOME=.
 
-set JAVA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y
+set JAVA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n
 %RED5_HOME%\red5.bat

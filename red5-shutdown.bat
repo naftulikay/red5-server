ed867b81b4dd1d3e7c46beb9011060b134d3b787
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2009-02-03 23:09:38 +0000

    reverted back to calling red5.bat after setting props for shutdown

--- red5-shutdown.bat
+++ red5-shutdown.bat
@@ -8,6 +8,6 @@ set JAVA_OPTS=-Djavax.net.ssl.keyStore=%RED5_HOME%conf\keystore.jmx -Djavax.net.
 set RED5_MAINCLASS=org.red5.server.Shutdown
 set RED5_OPTS=9999 red5user changeme
 
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp %RED5_CLASSPATH% %RED5_MAINCLASS% %RED5_OPTS%
+%RED5_HOME%\red5.bat
 
 ENDLOCAL

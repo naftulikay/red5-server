3cddefa7aa09ff0cedba46bd1537445bcc5c1ed0
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2009-08-29 02:56:00 +0000

    Simple plugin that only logs

---
+++ src/org/red5/server/plugin/DummyPlugin.java
@@ -0,0 +1,67 @@
+package org.red5.server.plugin;
+
+/*
+ * RED5 Open Source Flash Server - http://www.osflash.org/red5
+ * 
+ * Copyright (c) 2006-2009 by respective authors (see below). All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+import org.java.plugin.Plugin;
+import org.red5.logging.Red5LoggerFactory;
+import org.red5.server.Server;
+import org.red5.server.api.plugin.IRed5Plugin;
+import org.slf4j.Logger;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Simple plug-in to test functionality.
+ * 
+ * @author Paul Gregoire
+ */
+public class DummyPlugin extends Plugin implements IRed5Plugin {
+
+	private static Logger log = Red5LoggerFactory.getLogger(DummyPlugin.class, "plugins");
+	
+	@SuppressWarnings("unused")
+	private ApplicationContext context;
+	
+	@SuppressWarnings("unused")
+	private Server server;
+	
+	@Override
+	protected void doStart() throws Exception {
+		log.debug("Start");
+	}
+
+	@Override
+	protected void doStop() throws Exception {
+		log.debug("Stop");
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) {
+		log.debug("Set application context: {}", context);
+		this.context = context;
+	}
+
+	@Override
+	public void setServer(Server server) {
+		log.debug("Set server: {}", server);
+		this.server = server;
+	}
+	
+	
+}
